f108960ff497bb7c36c4dabc091c8517f4ffeaa6
Ivan <prigulnov88@gmail.com>
2023-09-12 21:03:52 +0500

    class Sides added

--- parsing_exel/parser/models.py
+++ parsing_exel/parser/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 
 class Billboards(models.Model):
-    LIGHTING = ['Да', 'Нет']
-    PRODUCT_RESTRICTIONS = ['Алкогольные напитки',
-                            'Табачные изделия,Алкогольные напитки']
+    LIGHTING = [
+        ('Да', 'Да'),
+        ('Нет', 'Нет')
+    ]
+    PRODUCT_RESTRICTIONS = [
+        ('Алкогольные напитки', 'Алкогольные напитки'),
+        ('Табачные изделия,Алкогольные напитки', 'Табачные изделия,Алкогольные напитки')
+    ]
 
-    city = models.CharField(db_column='Города', max_length=20, verbose_name='Город')
+    city = models.CharField(db_column='Города', max_length=20, verbose_name='Города')
     surface_type = models.CharField(db_column='Тип поверхности', max_length=50, verbose_name='Тип поверхности')
     lighting = models.CharField(db_column='Осв', max_length=3, choices=LIGHTING, verbose_name='Освещение')
     address = models.CharField(db_column='Адрес', max_length=250, verbose_name='Адрес')
@@ -15,11 +20,12 @@ class Billboards(models.Model):
     product_restrictions = models.CharField(db_column='Ограничения по продукту', null=True, max_length=40, choices=PRODUCT_RESTRICTIONS, verbose_name='Ограничения по продукту')
     district = models.CharField(db_column='Городской округ', null=True, max_length=100, verbose_name='Городской округ')
     permission = models.DateField(db_column='Разрешение ПО', null=True, verbose_name='Разрешение ПО')
+    # разобраться с форматом вводимых и автосоздаваемых дат
     date_create = models.DateTimeField(db_column='Дата создания', auto_now_add=True, verbose_name='Дата создания')
     date_update = models.DateTimeField(db_column='Дата обновления', auto_now=True, verbose_name='Дата обновления')
 
     def __str__(self):
-        return f'{self.city} {self.address}'
+        return f'{self.city}, {self.address}'
 
     class Meta:
         db_table = 'Билборды'
@@ -32,4 +38,28 @@ class Billboards(models.Model):
         unique_together = [
             ['city', 'address'],
             ['latitude', 'longitude']
+        ]
+
+
+class Sides(models.Model):
+    billboard = models.ForeignKey(Billboards, on_delete=models.CASCADE)
+    side = models.CharField(db_column='Сторона', max_length=6, verbose_name='Сторона')
+    internal_code = models.CharField(db_column='Вн. код', max_length=20, unique=True, verbose_name='Вн. код')
+    photo_or_scheme = models.URLField(db_column='Фото/схема', verbose_name='Фото/схема')
+    digital_views = models.PositiveSmallIntegerField(db_column='Диджитал кол-во показов', null=True, verbose_name='Диджитал кол-во показов')
+    media_material = models.CharField(db_column='Материал носителя', max_length=100, null=True, verbose_name='Материал носителя')
+    technical_requirements = models.URLField(db_column='Тех. требования', null=True, verbose_name='Тех. требования')
+    note = models.TextField(db_column='Примечение', null=True, verbose_name='Примечение')
+    date_create = models.DateTimeField(db_column='Дата создания', auto_now_add=True, verbose_name='Дата создания')
+    date_update = models.DateTimeField(db_column='Дата обновления', auto_now=True, verbose_name='Дата обновления')
+
+    def __str__(self):
+        return self.internal_code
+
+    class Meta:
+        db_table = 'Стороны'
+        verbose_name = 'Сторона'
+        verbose_name_plural = 'Стороны'
+        indexes = [
+            models.Index(fields=['internal_code'], name='internal_code_idx'),
         ]
