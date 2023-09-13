96e6c2fae9a63698148a584cb305f5df57ae2518
Ivan <prigulnov88@gmail.com>
2023-09-13 16:01:11 +0500

    Setting up the admin panel.

--- parsing_exel/parser/admin.py
+++ parsing_exel/parser/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class BillboardsAdmin(admin.ModelAdmin):
+    list_display = [
+        'city', 'address', 'surface_type', 'lighting', 'product_restrictions',
+        'permission',
+              ]
+
+
+class SidesAdmin(admin.ModelAdmin):
+    list_display = [
+        'internal_code', 'billboard', 'side', 'photo_or_scheme',
+        'digital_views', 'media_material', 'note', 'date_update'
+              ]
+
+
+class ESPARAdmin(admin.ModelAdmin):
+    list_display = [
+        'ESPAR_code', 'GRP', 'OTS', 'billboard', 'side', 'date_update',
+              ]
+
+
+class SalesAdmin(admin.ModelAdmin):
+    list_display = [
+        'year', 'month', 'status', 'billboard', 'side', 'ESPAR', 'date_update'
+              ]
+    list_filter = ['year', 'month', 'status']
+
+
+admin.site.register(Billboards, BillboardsAdmin)
+admin.site.register(Sides, SidesAdmin)
+admin.site.register(ESPAR, ESPARAdmin)
+admin.site.register(Sales, SalesAdmin)
+
+
+
+
