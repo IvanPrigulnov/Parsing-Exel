824169ed242a29f40c433df2fe2b9a3dedc23db3
Ivan <prigulnov88@gmail.com>
2023-09-14 17:50:50 +0500

    tuning admin panel

--- parsing_exel/parser/admin.py
+++ parsing_exel/parser/admin.py
@@ -1,35 +1,50 @@
 from django.contrib import admin
+
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
+from import_export.fields import Field
 from .models import *
 
 
-class BillboardsAdmin(admin.ModelAdmin):
-    list_display = [
-        'city', 'address', 'surface_type', 'lighting', 'product_restrictions',
-        'permission',
-              ]
+class BillboardsResources(resources.ModelResource):
+    id = Field(attribute='id', column_name='ID Билборда')
+    city = Field(attribute='city', column_name='Города')
+    surface_type = Field(attribute='surface_type', column_name='Тип поверхности')
+    lighting = Field(attribute='lighting', column_name='Освещение')
+    address = Field(attribute='address', column_name='Адрес')
+    latitude = Field(attribute='latitude', column_name='Широта')
+    longitude = Field(attribute='longitude', column_name='Долгота')
+    product_restrictions = Field(attribute='product_restrictions', column_name='Ограничения по продукту')
+    district = Field(attribute='district', column_name='Городской округ')
+    permission = Field(attribute='permission', column_name='Разрешение ПО')
+
+    class Meta:
+        model = Billboards
+        exclude = ('date_create', 'date_update')
 
 
-class SidesAdmin(admin.ModelAdmin):
-    list_display = [
-        'internal_code', 'billboard', 'side', 'photo_or_scheme',
-        'digital_views', 'media_material', 'note', 'date_update'
-              ]
-
-
-class ESPARAdmin(admin.ModelAdmin):
-    list_display = [
-        'ESPAR_code', 'GRP', 'OTS', 'billboard', 'side', 'date_update',
-              ]
-
-
-class SalesAdmin(admin.ModelAdmin):
-    list_display = [
-        'year', 'month', 'status', 'billboard', 'side', 'ESPAR', 'date_update'
-              ]
-    list_filter = ['year', 'month', 'status']
+class BillboardsAdmin(ImportExportModelAdmin):
+    list_display = [field.name for field in Billboards._meta.fields if field.name != 'id']
+    resource_classes = [BillboardsResources]
 
 
 admin.site.register(Billboards, BillboardsAdmin)
+
+
+class SidesAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Sides._meta.fields if field.name != 'id']
+
+
+class ESPARAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in ESPAR._meta.fields if field.name != 'id']
+
+
+class SalesAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Sales._meta.fields if field.name != 'id']
+    list_filter = ['year', 'month', 'status']
+
+
+
 admin.site.register(Sides, SidesAdmin)
 admin.site.register(ESPAR, ESPARAdmin)
 admin.site.register(Sales, SalesAdmin)
